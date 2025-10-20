91f3c5d4b894fa51d5aeac329ca0eb3b0ac4a40c
noellefox <143116149+noellefox@users.noreply.github.com>
2025-10-20 12:36:04 -0500

    Add unit tests for fibonacci function

---
+++ test_fib.py
@@ -0,0 +1,16 @@
+from fib import fibonacci  # replace with your actual filename
+
+def test_fib0():
+    assert fibonacci(0) == 0
+
+def test_fib1():
+    assert fibonacci(1) == 1
+
+def test_fib2():
+    assert fibonacci(2) == 1
+
+def test_fib5():
+    assert fibonacci(5) == 5
+
+def test_fib10():
+    assert fibonacci(10) == 55
